9d907a85310d9135ba2d821c99ef468f01c7e290
Paul Seal <p.seal@clerkswell.com>
2023-07-10 09:00:01 +0100

    moved connection strings

--- MyProject/Composers/JobsComposer.cs
+++ MyProject/Composers/JobsComposer.cs
@@ -1,26 +1,22 @@
 ﻿using Hangfire;
-using Hangfire.Common;
-using Hangfire.Console;
-using Hangfire.Server;
 using MyProject.Services;
 using Umbraco.Cms.Core.Composing;
 
-namespace MyProject.Composers
-{
-    public class JobsComposer : IComposer
-    {
-        public void Compose(IUmbracoBuilder builder)
-        {
-            if(AllowRunningHangfireJobs(builder))
-            {
-                RecurringJob.AddOrUpdate<IImportService>("Import", x => x.Import(), "*/1 * * * *");
-            }
-        }
+namespace MyProject.Composers;
 
-        private bool AllowRunningHangfireJobs(IUmbracoBuilder builder)
+public class JobsComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        if(AllowRunningHangfireJobs(builder))
         {
-            var config = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
-            return config.GetValue<bool>("AllowRunningHangfireJobs");
+            RecurringJob.AddOrUpdate<IImportService>("Import", x => x.Import(), "*/1 * * * *");
         }
+    }
+
+    private bool AllowRunningHangfireJobs(IUmbracoBuilder builder)
+    {
+        var config = builder.Services.BuildServiceProvider().GetService<IConfiguration>();
+        return config.GetValue<bool>("AllowRunningHangfireJobs");
     }
 }
